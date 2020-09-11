d66388d9677eb7f7c2ee12f1c0747013da3fffc2
MattiaRiola <s225629@studenti.polito.it>
2020-09-11 19:40:33 +0200

    code: setup clion project

---
+++ TimerCLionProject/cmake-build-debug/CMakeFiles/TimerCLionProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TimerCLionProject.dir/main.cpp.o"
+  "TimerCLionProject"
+  "TimerCLionProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TimerCLionProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
